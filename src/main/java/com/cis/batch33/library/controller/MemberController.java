0474bec96e00c7fe7db0bbcac18025e91f479b40
Lavanya <1999lavanyakona@gmail.com>
2024-02-19 16:29:21 -0600

    LibraryService Application

--- src/main/java/com/cis/batch33/library/controller/MemberController.java
+++ src/main/java/com/cis/batch33/library/controller/MemberController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/member")
 public class MemberController {
-
     @Autowired
     private MemberService memberService;
 
